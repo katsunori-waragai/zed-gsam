fd8e0b28f9d8ff25291bfa257d8387b91ad2ee8a
katsunori-waragai <huyuhiko1128@gmail.com>
2024-05-27 18:40:47 +0900

    test for numpy version

--- gsam.py
+++ gsam.py
@@ -64,7 +64,8 @@ if __name__ == "__main__":
         masks = gsam_predictor.masks
 
         t6 = cv2.getTickCount()
-        colorized = colorize_torch(gen_mask_img(masks)).cpu().numpy()
+        colorized = colorize(gen_mask_img(masks).numpy())
+        # colorized = colorize_torch(gen_mask_img(masks)).cpu().numpy()
         output_mask_jpg = output_dir / f"{image_path_stem}_mask.jpg"
         cv2.imwrite(str(output_mask_jpg), colorized)
         mask_json = output_mask_jpg.with_suffix(".json")
