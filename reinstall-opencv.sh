044eefd47fbeadc93d1fc1492423675b5b486445
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-05-27 13:43:24 +0900

    added reinstall-opencv.sh

--- reinstall-opencv.sh
+++ reinstall-opencv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 -m pip uninstall -y opencv-python opencv-python-headless opencv-contrib-python
-python3 -m pip instal -y opencv-python==3.4.18.65
+python3 -m pip install -y opencv-python==3.4.18.65
 
 
