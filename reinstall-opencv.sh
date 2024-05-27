35c6b494d21b01e7298282dc6412c626b9b16ac2
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-05-27 16:29:54 +0900

    reinstall opencv

--- reinstall-opencv.sh
+++ reinstall-opencv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 -m pip uninstall --yes opencv-python-headless opencv-contrib-python
+python3 -m pip uninstall --yes opencv-python-headless opencv-contrib-python opencv-python
 python3 -m pip install opencv-python==3.4.18.65
