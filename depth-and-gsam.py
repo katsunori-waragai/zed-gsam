09068099b48d29f64f7942281226478a25d68b3b
katsunori-waragai <huyuhiko1128@gmail.com>
2024-05-28 12:18:01 +0900

    developing depth-andgsam.py

--- depth-and-gsam.py
+++ depth-and-gsam.py
@@ -118,6 +118,7 @@ def main():
                 print(f"{cvimg.shape=}")
                 cvimg_bgr = cvimg[:, :, :3].copy()
                 gsam_predictor.infer_all(cvimg_bgr)
+                masks = gsam_predictor.masks
                 colorized = gsam_module.colorize_torch(gsam_module.gen_mask_img(masks)).cpu().numpy()
                 pred_phrases = gsam_predictor.pred_phrases
                 boxes_filt = gsam_predictor.boxes_filt
