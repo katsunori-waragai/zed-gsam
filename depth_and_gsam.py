829b9751d0ae9e611dc4626e7cffabe408d2a648
katsunori-waragai <huyuhiko1128@gmail.com>
2024-05-31 15:31:20 +0900

    still under try

--- depth_and_gsam.py
+++ depth_and_gsam.py
@@ -218,15 +218,15 @@ def main():
                 plt.xlabel("x [m]")
                 plt.xlabel("y [m]")
 
-                fig, ax = plt.subplot(2, 2, 2)
+                plt.subplot(2, 2, 2)
                 for i, (selected, phrase) in enumerate(zip(selected_list, pred_phrases)):
                     if phrase.find("bottle") > -1:
                         x = selected[:, 0]
                         y = selected[:, 1]
                         z = selected[:, 2]
-                        sc = ax.scatter(x, y, c=z, cmap='viridis')
+                        sc = plt.scatter(x, y, c=z, cmap='viridis')
 
-                plt.colorbar(sc, ax=ax, label='Z Value')
+                plt.colorbar(sc, label='Z Value')
                 plt.show()
 
                 plt.subplot(2, 2, 3)
