cdbc66203ad3d7176bc95a46b0708a8b22e164c2
katsunori-waragai <huyuhiko1128@gmail.com>
2024-06-03 17:22:56 +0900

    still under develop

--- depth_and_gsam.py
+++ depth_and_gsam.py
@@ -276,7 +276,7 @@ def main():
                 print(f"{is_picked.shape=}")
                 print(f"{depth_map_img.shape=} {depth_map_img.dtype=}")
                 # float型で標準化する。遠方ほどマイナスになる座標系なので, np.abs()を利用する
-                normalized_depth = np.clip(np.abs(depth_map_img[:, :, 0]) / (MAX_DEPTH - MIN_DEPTH), 0.0, 1.0)
+                normalized_depth = np.clip(np.abs(depth_map_img) / (MAX_DEPTH - MIN_DEPTH), 0.0, 1.0)
                 print(f"{normalized_depth.shape=} {normalized_depth.dtype=}")
                 # float型からjetの擬似カラーに変更する。
                 pseudo_color_depth = matplotlib.cm.jet(normalized_depth)
