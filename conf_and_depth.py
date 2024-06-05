479e27f858febf033302e76823e5fea3ac41ac12
katsunori-waragai <huyuhiko1128@gmail.com>
2024-06-05 15:41:50 +0900

    use parse_args

--- conf_and_depth.py
+++ conf_and_depth.py
@@ -10,8 +10,49 @@ import cv2
 import numpy as np
 
 from zedhelper import predefined
-from depth_and_gsam import parse_args
+
 import inspect
+
+def parse_args(init):
+    if len(opt.input_svo_file) > 0 and opt.input_svo_file.endswith(".svo"):
+        init.set_from_svo_file(opt.input_svo_file)
+        print("[Sample] Using SVO File input: {0}".format(opt.input_svo_file))
+    elif len(opt.ip_address) > 0:
+        ip_str = opt.ip_address
+        if (
+            ip_str.replace(":", "").replace(".", "").isdigit()
+            and len(ip_str.split(".")) == 4
+            and len(ip_str.split(":")) == 2
+        ):
+            init.set_from_stream(ip_str.split(":")[0], int(ip_str.split(":")[1]))
+            print("[Sample] Using Stream input, IP : ", ip_str)
+        elif ip_str.replace(":", "").replace(".", "").isdigit() and len(ip_str.split(".")) == 4:
+            init.set_from_stream(ip_str)
+            print("[Sample] Using Stream input, IP : ", ip_str)
+        else:
+            print("Unvalid IP format. Using live stream")
+    if "HD2K" in opt.resolution:
+        init.camera_resolution = sl.RESOLUTION.HD2K
+        print("[Sample] Using Camera in resolution HD2K")
+    elif "HD1200" in opt.resolution:
+        init.camera_resolution = sl.RESOLUTION.HD1200
+        print("[Sample] Using Camera in resolution HD1200")
+    elif "HD1080" in opt.resolution:
+        init.camera_resolution = sl.RESOLUTION.HD1080
+        print("[Sample] Using Camera in resolution HD1080")
+    elif "HD720" in opt.resolution:
+        init.camera_resolution = sl.RESOLUTION.HD720
+        print("[Sample] Using Camera in resolution HD720")
+    elif "SVGA" in opt.resolution:
+        init.camera_resolution = sl.RESOLUTION.SVGA
+        print("[Sample] Using Camera in resolution SVGA")
+    elif "VGA" in opt.resolution:
+        init.camera_resolution = sl.RESOLUTION.VGA
+        print("[Sample] Using Camera in resolution VGA")
+    elif len(opt.resolution) > 0:
+        print("[Sample] No valid resolution entered. Using default")
+    else:
+        print("[Sample] Using default resolution")
 
 
 def any_isnan(array: np.ndarray) -> bool:
