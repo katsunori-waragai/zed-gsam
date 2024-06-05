15d60cf8ee451f22e691a77b84841eadae289252
katsunori-waragai <huyuhiko1128@gmail.com>
2024-06-05 15:35:28 +0900

    use parse_args

--- conf_and_depth.py
+++ conf_and_depth.py
@@ -77,10 +77,31 @@ def main(opt):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
+        "--input_svo_file",
+        type=str,
+        help="Path to an .svo file, if you want to replay it",
+        default="",
+    )
+    parser.add_argument(
+        "--ip_address",
+        type=str,
+        help="IP Adress, in format a.b.c.d:port or a.b.c.d, if you have a streaming setup",
+        default="",
+    )
+    parser.add_argument(
+        "--resolution",
+        type=str,
+        help="Resolution, can be either HD2K, HD1200, HD1080, HD720, SVGA or VGA",
+        default="",
+    )
+    parser.add_argument(
         "--confidence_threshold",
         type=float,
         help="depth confidence_threshold(0 ~ 100)",
         default=100,
     )
     opt = parser.parse_args()
+    if len(opt.input_svo_file) > 0 and len(opt.ip_address) > 0:
+        print("Specify only input_svo_file or ip_address, or none to use wired camera, not both. Exit program")
+        exit()
     main(opt)
