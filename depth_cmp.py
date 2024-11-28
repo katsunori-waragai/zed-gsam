7ac013158002cd2ad4d440f6efe82defdc0c1abf
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-11-28 17:53:29 +0900

    Feature/use common 2 (#36)
    what
    共通部分をimport に置換えた。
    わかったこと
    colorに対して以下の処理をすることは無意味なことが判明した。
    valid_points_mask = np.isfinite(points_color)
    depth_cmp.py
    自体も不要であろう。

--- depth_cmp.py
+++ depth_cmp.py
@@ -15,51 +15,11 @@ import matplotlib.pyplot as plt
 import zedhelper.handmark
 import zedhelper.util
 from zedhelper import predefined
+import common
 
 import inspect
 
-MAX_ABS_DEPTH, MIN_ABS_DEPTH = 0.0, 2.0  # [m]
-
-def parse_args(init):
-    if len(opt.input_svo_file) > 0 and opt.input_svo_file.endswith(".svo"):
-        init.set_from_svo_file(opt.input_svo_file)
-        print("[Sample] Using SVO File input: {0}".format(opt.input_svo_file))
-    elif len(opt.ip_address) > 0:
-        ip_str = opt.ip_address
-        if (
-            ip_str.replace(":", "").replace(".", "").isdigit()
-            and len(ip_str.split(".")) == 4
-            and len(ip_str.split(":")) == 2
-        ):
-            init.set_from_stream(ip_str.split(":")[0], int(ip_str.split(":")[1]))
-            print("[Sample] Using Stream input, IP : ", ip_str)
-        elif ip_str.replace(":", "").replace(".", "").isdigit() and len(ip_str.split(".")) == 4:
-            init.set_from_stream(ip_str)
-            print("[Sample] Using Stream input, IP : ", ip_str)
-        else:
-            print("Unvalid IP format. Using live stream")
-    if "HD2K" in opt.resolution:
-        init.camera_resolution = sl.RESOLUTION.HD2K
-        print("[Sample] Using Camera in resolution HD2K")
-    elif "HD1200" in opt.resolution:
-        init.camera_resolution = sl.RESOLUTION.HD1200
-        print("[Sample] Using Camera in resolution HD1200")
-    elif "HD1080" in opt.resolution:
-        init.camera_resolution = sl.RESOLUTION.HD1080
-        print("[Sample] Using Camera in resolution HD1080")
-    elif "HD720" in opt.resolution:
-        init.camera_resolution = sl.RESOLUTION.HD720
-        print("[Sample] Using Camera in resolution HD720")
-    elif "SVGA" in opt.resolution:
-        init.camera_resolution = sl.RESOLUTION.SVGA
-        print("[Sample] Using Camera in resolution SVGA")
-    elif "VGA" in opt.resolution:
-        init.camera_resolution = sl.RESOLUTION.VGA
-        print("[Sample] Using Camera in resolution VGA")
-    elif len(opt.resolution) > 0:
-        print("[Sample] No valid resolution entered. Using default")
-    else:
-        print("[Sample] Using default resolution")
+MAX_ABS_DEPTH, MIN_ABS_DEPTH = 0.0, 5.0  # [m]
 
 
 def resize_image(image: np.ndarray, rate: float) -> np.ndarray:
@@ -97,7 +57,7 @@ def main(opt):
 
     init_params = predefined.InitParameters()
 
-    parse_args(init_params)
+    common.parse_args(opt, init_params)
 
     init_params.depth_mode = sl.DEPTH_MODE.ULTRA
 
@@ -160,7 +120,7 @@ def main(opt):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="depth map viewer")
     parser.add_argument(
         "--input_svo_file",
         type=str,
